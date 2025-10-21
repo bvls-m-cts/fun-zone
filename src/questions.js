d1207c1bd86444422875b9b040a061096d21f845
bvls-m-cts <bhargavavenkatalakshmisai.mandalaneni@cognizant.com>
2025-10-21 11:27:19 +0530

    Folder re-arrangements

--- src/questions.js
+++ src/questions.js
@@ -1,200 +1,146 @@
+
 export const puzzles = {
   all: [
     {
-      type: 'multiple-choice',
-      question: "I’m there when your tire goes flat, Or when your engine says, “That’s that!.”\nI’m not a mechanic, but I send one fast.\nWho am I, making breakdowns a thing of the past?",
-      options: ['a. Car Dealership', 'b. GPS Navigation', 'c. Vehicle Warranty', 'd. AA Roadside Assistance'],
-      hint: '', 
-      answer: 'd. AA Roadside Assistance',
-      explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "If you had cynophobia, which animal would scare you?",
-      options: ['a. Dogs', 'b. Cats', 'c. Birds', 'd. Snakes'],
-      hint: '', 
-      answer: 'a. Dogs',
-      explanation: 'Cyno is the Greek word for dog. Cynophobia is the medical term for an extreme and irrational fear of dogs.'
-    },
-    {
       type: 'image-choice',
-      question: 'Guess the word?',
+      question: 'Guess the two animals from the combo pic',
       images: [
-        'public/quiz/alphabet/P.jpg',
-        'public/quiz/alphabet/E.jpg',
-        'public/quiz/alphabet/A.jpg',
-        'public/quiz/alphabet/L.jpg',
-        'public/quiz/alphabet/S.jpg',
-        'public/quiz/alphabet/P.jpg'
+        'public/Animal1.png'
     ],
-    hint: 'I will keep the doctor away',
-    answer: 'APPLES',
-    explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "I find what others miss in logs, in code, in every abyss.\nI break things just to make them right, and sleep with test cases every night.\nWho am I?",
-      options: ['a. Developer', 'b. Tester', 'c. Scrum Master', 'd. Product Owner'],
-      hint: '', 
-      answer: 'b. Tester',
-      explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "The speed of a computer mouse is measured in what units?",
-      options: ['a. Inches', 'b. Paws', 'c. Centimeters', 'd. Mickeys'],
-      hint: '', 
-      answer: 'd. Mickeys',
-      explanation: 'The unit of measurement for the speed and movement of a computer mouse is the "Mickey," with one Mickey being equivalent to approximately 1/100th of an inch.'
+    hint: '',  
+    answer: 'Elephant/Tiger'      
     },
     {
       type: 'image-choice',
-      question: 'Guess the Scientist?',
+      question: 'I speak without a mouth and hear without ears.I have no body, but I come alive with wind.What am I?',
       images: [
-        'public/quiz/Albert_Einstein.jpg'
+        'public/QuestionMark.png'
     ],
-    hint: 'He is known for his “Theory of Relativity”',
-    answer: 'Albert Einstein',
-    explanation: 'Albert Einstein as a boy. This photo was taken in 1884, at the age 5.'
-    },
-    {
-      type: 'multiple-choice',
-      question: "On which day of the week are workers most statistically likely to call in sick?",
-      options: ['a. Friday', 'b. Tuesday', 'c. Wednesday', 'd. Monday'],
-      hint: '', 
-      answer: 'd. Monday',
-      explanation: 'This phenomenon is often attributed to the "Monday blues" or the transition from the weekend to the workweek.'
-    },
-    {
-      type: 'multiple-choice',
-      question: "What is the only food that never goes bad?",
-      options: ['a. Honey', 'b. Corn', 'c. Pickles', 'd. Rice'],
-      hint: '', 
-      answer: 'a. Honey',
-      explanation: 'Honey is naturally acidic and has a low moisture content, which makes it an inhospitable environment for bacteria and other microorganisms.'
-    },
-    {
-      type: 'direct-answer',
-      question: 'Solve the Riddle.\nIt has keys, but no locks. It has space, but no room. You can enter, but can’t go inside. What is it?',
-      hint: '',
-      answer: 'A keyboard',
-      explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "In Harry Potter, what is the name of Harry’s pet owl?",
-      options: ['a. Crookshanks', 'b. Hedwig', 'c. Scabbers', 'd. Errol'],
-      hint: '', 
-      answer: 'b. Hedwig',
-      explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "What sea creatures have been used in wars?",
-      options: ['a. Jellyfish', 'b. Sharks', 'c. Dolphins', 'd. Otters'],
-      hint: '', 
-      answer: 'c. Dolphins',
-      explanation: 'Dolphins have been trained by military forces for tasks such as detecting mines, identifying enemy divers, and recovering equipment.'
+    hint: '',  
+    answer: 'An Echo'      
     },
     {
       type: 'image-choice',
-      question: "What's wrong here?",
+      question: 'Guess the two animals from the combo pic',
       images: [
-        'public/quiz/wwh_3.jpg'
+        'public/Animal2.png'
+    ],
+    hint: '',  
+    answer: 'Tiger/Parrot'      
+    },
+    {
+      type: 'image-choice',
+      question: 'I have cities, but no houses.I have mountains, but no trees.I have water, but no fish.What am I?',
+      images: [
+        'public/QuestionMark.png'
+    ],
+    hint: '',  
+    answer: 'A Map'      
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the two animals from the combo pic',
+      images: [
+        'public/Animal3.png'
+    ],
+    hint: '',  
+    answer: 'Fox/Cat'      
+    },
+    {
+      type: 'image-choice',
+      question: 'The more you take, the more you leave behind.What am I?',
+      images: [
+        'public/QuestionMark.png'
     ],
     hint: '',
-    answer: 'It’s Almond milk, but there’s a cow on it',
-    explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "Which country has won the most FIFA World Cups?",
-      options: ['a. Germany', 'b. Argentina', 'c. Brazil', 'd. Italy'],
-      hint: '', 
-      answer: 'c. Brazil',
-      explanation: 'Brazil has won 5 World Cups: 1958, 1962, 1970, 1994, 2002'
+    answer: 'Footsteps'      
     },
     {
       type: 'image-choice',
-      question: 'Guess the monument?',
+      question: 'Which animal owns this eye?',
       images: [
-        'public/quiz/Stonehenge.jpg'
+        'public/Animal4.png'
     ],
-    hint: 'Built by hands long gone, still standing strong.',
-    answer: 'Stonehenge',
-    explanation: 'Stonehenge is a prehistoric monument in England, built over 4,000 years ago. Its purpose remains a mystery, but it’s believed to have been used for ceremonies or astronomy.'
-    },
-    {
-      type: 'multiple-choice',
-      question: "I’m famous for circles, not squares, Drivers here must be quite aware.\nWith more roundabouts than you’d expect, What town am I, with traffic unchecked?",
-      options: ['a. Swindon', 'b. Basingstoke', 'c. Oxford', 'd. Reading'],
-      hint: '', 
-      answer: 'b. Basingstoke',
-      explanation: ''
+    hint: '',  
+    answer: 'Tiger'      
     },
     {
       type: 'image-choice',
-      question: "What's wrong here?",
+      question: 'I am a five-letter word and I am feared by many.If you remove my first letter, I become kind.What am I?',
       images: [
-        'public/quiz/wwh_1.jpg'
+        'public/QuestionMark.png'
     ],
     hint: '',
-    answer: 'This wire is unplugged but still the lamp is on',
-    explanation: ''
-    },
-    {
-      type: 'multiple-choice',
-      question: "In Tom and Jerry, what is Jerry?",
-      options: ['a. A Cat', 'b. A Dog', 'c. A Mouse', 'd. A Rabbit'],
-      hint: '', 
-      answer: 'c. A Mouse',
-      explanation: ''
+    answer: 'Scare'      
     },
     {
       type: 'image-choice',
-      question: 'Guess the word?',
+      question: 'Which animal owns this eye?',
       images: [
-        'public/quiz/alphabet/D.jpg',
-        'public/quiz/alphabet/W.jpg',
-        'public/quiz/alphabet/R.jpg',
-        'public/quiz/alphabet/A.jpg',
-        'public/quiz/alphabet/O.jpg',
-        'public/quiz/alphabet/Y.jpg',
-        'public/quiz/alphabet/A.jpg',
+        'public/Animal5.png'
     ],
-    hint: 'It’s paved for tires, not wings, waves, or rails.',
-    answer: 'ROADWAY',
-    explanation: ''
+    hint: '',  
+    answer: 'Cat'      
     },
     {
-      type: 'multiple-choice',
-      question: "I don’t wear a badge, yet I’m followed. I don’t give orders, yet I’m obeyed.\nI inspire without speaking, and I’m strongest when shared. What am I?",
-      options: ['a. Management', 'b. Leadership', 'c. Communication', 'd. Strategy'],
-      hint: '', 
-      answer: 'b. Leadership',
-      explanation: ''
+      type: 'image-choice',
+      question: 'Which animal owns this ear?',
+      images: [
+        'public/Animal6.png'
+    ],
+    hint: '',  
+    answer: 'Pig'      
     },
     {
-      type: 'direct-answer',
-      question: 'Solve the Riddle.\nI am the tool you hate to receive, but also can’t stand to lose. My presence marks your absence.',
-      hint: '',
-      answer: 'An out-of-office message',
-      explanation: ''
+      type: 'image-choice',
+      question: 'What comes down but never goes up?',
+      images: [
+        'public/QuestionMark.png'
+    ],
+    hint: '',
+    answer: 'Rain'      
     },
     {
-      type: 'multiple-choice',
-      question: "What is the name of the little dot seen above the alphabets ‘j’ and the ‘i’?",
-      options: ['a. A Dot', 'b. A Tittle', 'c. A Jot', 'd. A Spot'],
-      hint: '', 
-      answer: 'b. A Tittle',
-      explanation: 'A tittle is a small distinguishing mark, such as a diacritic in a foreign language or the dot on a lowercase i or j.'
+      type: 'image-choice',
+      question: 'Guess the two celebrities in this combo pic?',
+      images: [
+        'public/Celebrity1.png'
+    ],
+    hint: '',  
+    answer: 'Lionel Messi/Leonardo DiCaprio'      
+    },
+    {
+      type: 'image-choice',
+      question: 'If you have it, you want to share it.If you share it, you don’t have it.What is it?',
+      images: [
+        'public/QuestionMark.png'
+    ],
+    hint: '',
+    answer: 'A Secret'      
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the two celebrities in this combo pic?',
+      images: [
+        'public/Celebrity2.png'
+    ],
+    hint: '',  
+    answer: 'Christiano Ronaldo/Donald Trump'      
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the two celebrities in this combo pic?',
+      images: [
+        'public/Celebrity3.png'
+    ],
+    hint: '',  
+    answer: 'Albert Einstein/Isaac Newton'      
     }
-    
-  ]
+    ],
 };
 
 export const truthQuestions = [
-  "What’s a secret you’ve never told anyone?",
+ "What’s a secret you’ve never told anyone?",
   "If you could switch lives with one person for a day, who would it be?",
   "What’s the funniest thing that’s ever happened to you at work?",
   "What’s a skill you secretly admire in a colleague?",
