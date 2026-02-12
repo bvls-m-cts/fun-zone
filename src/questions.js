aa0bf98420157afc2a5c988d74e00f6acd269b6b
bvls-m-cts <bhargavavenkatalakshmisai.mandalaneni@cognizant.com>
2026-02-12 21:09:49 +0530

    13th Feb 2026 Updates

--- src/questions.js
+++ src/questions.js
@@ -1,135 +1,160 @@
 export const puzzles = {
-  all: [
+  all: [    
+    {
+      type: 'image-choice',
+      question: 'Guess the Emoji',
+      images: [
+        'public/quiz/q1.jpg'
+    ],
+    hint: '',
+    answer: 'Back-to-back meetings',
+    explanation: ''
+    },
     {
       type: 'direct-answer',
-      question: 'How many times does the letter ‘a’ appear when spelling out all the numbers from 1 to 100?',
+      question: 'Which fruit is known as the “King of Fruits”?',
       images: [],
       hint: '',
-      answer: 'Zero',
-      explanation: 'There are 0 occurrences of the letter “a” in the spelled-out numbers from 1 to 100 (in both American and British conventions)'
+      answer: 'Mango',
+      explanation: ''
     },
     {
       type: 'image-choice',
-      question: 'Image shows a rock‑cut cave complex in India, famous for Buddhist sculptures. Name it.',
+      question: 'Guess the Emoji',
       images: [
-        'public/quiz/Elephanta_Caves.png'
-    ],
-    hint: 'Located on an island, accessible by ferry from Mumbai.',
-    answer: 'Elephanta Caves',
-    explanation: 'The Elephanta Caves form a collection of cave temples predominantly dedicated to the Hindu god Shiva; UNESCO has designated them as a World Heritage Site. They are located on Elephanta Island, or Gharapuri, in Mumbai Harbour, 10 kilometres east of Mumbai in the Indian state of Maharashtra.'
-    },
-    {
-      type: 'image-choice',
-      question: 'In Jurassic Park movie, what animal’s DNA was mixed with dinosaur DNA to fill the gaps?',
-      images: [
-        'public/quiz/dinosaur.jpg'
+        'public/quiz/q3.jpg'
     ],
     hint: '',
-    answer: 'Frog',
+    answer: 'Lion King',
     explanation: ''
-    },    
+    }, 
     {
-      type: 'image-choice',
-      question: 'Which is the only flag in the world that is not rectangular or square?',
-      images: [
-        'public/quiz/Nepal.jpg'
-    ],
+      type: 'direct-answer',
+      question: 'What color is formed by mixing all three primary colors?',
+      images: [],
       hint: '',
-      answer: 'Nepal',
+      answer: 'Black (or dark brown)',
       explanation: ''
     },
     {
       type: 'image-choice',
-      question: 'Guess the festival that celebrated by throwing tomatoes at each other.',
+      question: 'Guess the Emoji',
       images: [
-        'public/quiz/La_Tomatina.jpg'
+        'public/quiz/q5.jpg'
     ],
-    hint: 'A spanish food fight festival',
-    answer: 'La Tomatina',
-    explanation: 'La Tomatina is an annual Spanish food fight festival held on the last Wednesday of August in the town of Buñol, near Valencia. Participants throw over 120,000 kilograms of tomatoes at each other in a massive, one-hour-long battle, which has become a major tourist attraction.'
+    hint: '',
+    answer: 'Finding Nemo',
+    explanation: ''
     },
     {
       type: 'direct-answer',
-      question: 'What is the only continent without reptiles or snakes?',
+      question: 'I am an odd number. Take away one letter and I become even. What number am I?',
+      images: [],
       hint: '',
-      answer: 'Antarctica',
+      answer: 'Seven (S-E-V-E-N minus "S" is "EVEN")',
       explanation: ''
     },
     {
       type: 'image-choice',
-      question: 'Which Indian state is home to the Valley of Flowers National Park, a UNESCO World Heritage Site?',
+      question: 'Guess the Emoji',
       images: [
-        'public/quiz/Uttarakhand.jpg'
+        'public/quiz/q7.jpg'
     ],
     hint: '',
-    answer: 'Uttarakhand',
-    explanation: 'It is located in Chamoli in the state of Uttarakhand and is known for its meadows of endemic alpine flowers and the variety of flora.'
-    },    
-    {
-      type: 'image-choice',
-      question: 'In Titanic, what was the name of the necklace that Rose had?',
-      images: [
-        'public/quiz/HeartOfOcean.jpg'
-    ],
-    hint: 'Also called the "Blue Diamond"',
-    answer: 'The Heart of the Ocean',
-    explanation: 'The name of the necklace Rose wore in Titanic was the Heart of the Ocean. This fictional blue diamond necklace was a gift from her fiancé, Caledon Hockley, and is a central element of the film’s plot.'
+    answer: 'Lunch break!',
+    explanation: ''
     },
     {
       type: 'direct-answer',
-      question: 'Solve the Riddle:\nI’m tall when I’m young, and short when I’m old. What am I?',
+      question: 'What has a head and a tail but no body?',
+      images: [],
       hint: '',
-      answer: 'A Candle',
+      answer: 'Coin',
       explanation: ''
     },
     {
       type: 'image-choice',
-      question: 'Which city is known as the “Pink City” of India?',
+      question: 'Guess the Emoji',
       images: [
-        'public/quiz/Jaipur.jpg'
+        'public/quiz/q9.jpg'
     ],
     hint: '',
-    answer: 'Jaipur',
-    explanation: 'The Pink City of India is Jaipur, the capital of Rajasthan. The nickname is due to the distinctive terracotta pink color of the buildings in its historic walled city area.'
-    },
-    {
-      type: 'image-choice',
-      question: 'Guess the place:',
-      images: [
-        'public/quiz/London/img1.jpg','public/quiz/London/img2.jpg','public/quiz/London/img3.jpg'
-    ],
-      hint: '',
-      answer: 'London, UK',
-      explanation: ''
-    },
-    {
-      type: 'image-choice',
-      question: 'In the movie Avatar, what is the valuable mineral that the humans are mining on Pandora?',
-      images: [
-        'public/quiz/Unobtanium.jpeg'
-    ],
-      hint: '',
-      answer: 'Unobtanium',
-      explanation: ''
-    },
-    {
-      type: 'image-choice',
-      question: 'Guess the festival:\nPeople wear costumes, carve pumpkins, and go trick‑or‑treating.',
-      images: [
-        'public/quiz/Halloween.jpg'
-    ],
-    hint: '',
-    answer: 'Halloween',
-    explanation: 'Halloween is celebrated on October 31st each year. It originated from the ancient Celtic festival of Samhain, where people would light bonfires and wear costumes to ward off ghosts. Today, it is known for activities like trick-or-treating, costume parties, and pumpkin carving.'
+    answer: 'Constant calls',
+    explanation: ''
     },
     {
       type: 'direct-answer',
-      question: 'Solve the Riddle:\nWhat has to be broken before you can use it?',
+      question: 'What word is spelled incorrectly in every single dictionary?',
+      images: [],
       hint: '',
-      answer: 'An Egg',
+      answer: 'Incorrectly',
       explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the Emoji',
+      images: [
+        'public/quiz/q11.jpg'
+    ],
+    hint: '',
+    answer: 'Bug fix',
+    explanation: ''
+    },
+    {
+      type: 'direct-answer',
+      question: 'Which artist has the most total streams on Spotify as of early 2026?',
+      images: [],
+      hint: '',
+      answer: 'Taylor Swift',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the Emoji',
+      images: [
+        'public/quiz/q13.jpg'
+    ],
+    hint: '',
+    answer: 'Cloud deployment',
+    explanation: ''
+    },
+    {
+      type: 'direct-answer',
+      question: 'What color do you get when you mix blue and yellow?',
+      images: [],
+      hint: '',
+      answer: 'Green',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the Emoji',
+      images: [
+        'public/quiz/q15.jpg'
+    ],
+    hint: '',
+    answer: 'Office shuttle',
+    explanation: ''
+    },
+    {
+      type: 'direct-answer',
+      question: 'Which month has 28 days?',
+      images: [],
+      hint: '',
+      answer: 'All of them',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the Emoji',
+      images: [
+        'public/quiz/q17.jpg'
+    ],
+    hint: '',
+    answer: 'Frozen',
+    explanation: ''
     }
-    
+
   ]
 };
 
