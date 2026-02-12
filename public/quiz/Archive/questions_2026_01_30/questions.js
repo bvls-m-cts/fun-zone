11ae6d20c0167b29f5a2f00aea613b93b466ff67
bvls-m-cts <bhargavavenkatalakshmisai.mandalaneni@cognizant.com>
2026-02-12 11:36:54 +0530

    Folder Restructuring

---
+++ public/quiz/Archive/questions_2026_01_30/questions.js
@@ -0,0 +1,160 @@
+export const puzzles = {
+  all: [
+    {
+      type: 'direct-answer',
+      question: 'How many times does the letter ‘a’ appear when spelling out all the numbers from 1 to 100?',
+      images: [],
+      hint: '',
+      answer: 'Zero',
+      explanation: 'There are 0 occurrences of the letter “a” in the spelled-out numbers from 1 to 100 (in both American and British conventions)'
+    },
+    {
+      type: 'image-choice',
+      question: 'Image shows a rock‑cut cave complex in India, famous for Buddhist sculptures. Name it.',
+      images: [
+        'public/quiz/Elephanta_Caves.png'
+    ],
+    hint: 'Located on an island, accessible by ferry from Mumbai.',
+    answer: 'Elephanta Caves',
+    explanation: 'The Elephanta Caves form a collection of cave temples predominantly dedicated to the Hindu god Shiva; UNESCO has designated them as a World Heritage Site. They are located on Elephanta Island, or Gharapuri, in Mumbai Harbour, 10 kilometres east of Mumbai in the Indian state of Maharashtra.'
+    },
+    {
+      type: 'image-choice',
+      question: 'In Jurassic Park movie, what animal’s DNA was mixed with dinosaur DNA to fill the gaps?',
+      images: [
+        'public/quiz/dinosaur.jpg'
+    ],
+    hint: '',
+    answer: 'Frog',
+    explanation: ''
+    },    
+    {
+      type: 'image-choice',
+      question: 'Which is the only flag in the world that is not rectangular or square?',
+      images: [
+        'public/quiz/Nepal.jpg'
+    ],
+      hint: '',
+      answer: 'Nepal',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the festival that celebrated by throwing tomatoes at each other.',
+      images: [
+        'public/quiz/La_Tomatina.jpg'
+    ],
+    hint: 'A spanish food fight festival',
+    answer: 'La Tomatina',
+    explanation: 'La Tomatina is an annual Spanish food fight festival held on the last Wednesday of August in the town of Buñol, near Valencia. Participants throw over 120,000 kilograms of tomatoes at each other in a massive, one-hour-long battle, which has become a major tourist attraction.'
+    },
+    {
+      type: 'direct-answer',
+      question: 'What is the only continent without reptiles or snakes?',
+      hint: '',
+      answer: 'Antarctica',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Which Indian state is home to the Valley of Flowers National Park, a UNESCO World Heritage Site?',
+      images: [
+        'public/quiz/Uttarakhand.jpg'
+    ],
+    hint: '',
+    answer: 'Uttarakhand',
+    explanation: 'It is located in Chamoli in the state of Uttarakhand and is known for its meadows of endemic alpine flowers and the variety of flora.'
+    },    
+    {
+      type: 'image-choice',
+      question: 'In Titanic, what was the name of the necklace that Rose had?',
+      images: [
+        'public/quiz/HeartOfOcean.jpg'
+    ],
+    hint: 'Also called the "Blue Diamond"',
+    answer: 'The Heart of the Ocean',
+    explanation: 'The name of the necklace Rose wore in Titanic was the Heart of the Ocean. This fictional blue diamond necklace was a gift from her fiancé, Caledon Hockley, and is a central element of the film’s plot.'
+    },
+    {
+      type: 'direct-answer',
+      question: 'Solve the Riddle:\nI’m tall when I’m young, and short when I’m old. What am I?',
+      hint: '',
+      answer: 'A Candle',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Which city is known as the “Pink City” of India?',
+      images: [
+        'public/quiz/Jaipur.jpg'
+    ],
+    hint: '',
+    answer: 'Jaipur',
+    explanation: 'The Pink City of India is Jaipur, the capital of Rajasthan. The nickname is due to the distinctive terracotta pink color of the buildings in its historic walled city area.'
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the place:',
+      images: [
+        'public/quiz/London/img1.jpg','public/quiz/London/img2.jpg','public/quiz/London/img3.jpg'
+    ],
+      hint: '',
+      answer: 'London, UK',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'In the movie Avatar, what is the valuable mineral that the humans are mining on Pandora?',
+      images: [
+        'public/quiz/Unobtanium.jpeg'
+    ],
+      hint: '',
+      answer: 'Unobtanium',
+      explanation: ''
+    },
+    {
+      type: 'image-choice',
+      question: 'Guess the festival:\nPeople wear costumes, carve pumpkins, and go trick‑or‑treating.',
+      images: [
+        'public/quiz/Halloween.jpg'
+    ],
+    hint: '',
+    answer: 'Halloween',
+    explanation: 'Halloween is celebrated on October 31st each year. It originated from the ancient Celtic festival of Samhain, where people would light bonfires and wear costumes to ward off ghosts. Today, it is known for activities like trick-or-treating, costume parties, and pumpkin carving.'
+    },
+    {
+      type: 'direct-answer',
+      question: 'Solve the Riddle:\nWhat has to be broken before you can use it?',
+      hint: '',
+      answer: 'An Egg',
+      explanation: ''
+    }
+    
+  ]
+};
+
+export const truthQuestions = [
+  "If you could instantly teleport anywhere, where would you go?",
+  "If you could swap jobs/role with anyone here for a day, who would it be?",
+  "What’s the funniest thing that’s ever happened to you at work?",
+  "Which colleague would you like to take along with you to survive a zombie apocalypse?",
+  "What’s a skill you secretly admire in a colleague?",
+  "Have you ever forgotten someone’s name right after they introduced themselves?",
+  "What’s your go-to excuse when you’re running late?",
+  "Have you ever accidentally sent a message to the wrong person on Teams or email?",
+  "If you could have a superpower for a day, what would it be?",
+  "If you could be any age for a week, what age would you be?"  
+];
+
+export const dareQuestions = [
+  "Speak a sentence in 2 different languages",
+  "Compliment three colleagues that you wish to",
+  "Tell a tongue twister as fast as you can - ‘She sells seashells by the seashore’ (or) ‘Peter piper picked a peck of pickled peppers’",
+  "Tell any nursery rhyme that you remember",  
+  "Pretend you’re giving a weather report for your current mood",
+  "Share a random fun fact you know — the weirder, the better!",
+  "Pretend you're a news anchor reporting on something funny that happened in the office",
+  "Explain the cooking process of your favourite dish in a minute",
+  "Tell a sentence by adding 'meow' before each word",
+  "Have a 15 seconds conversation with an object"
+];
